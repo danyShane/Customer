a81bc831d9e4d79951bd775912dab59bd6448078
danyShane <peruchan_clan@hotmail.com>
2019-07-03 10:33:46 +0200

    first commit upload files

---
+++ CustomerCsv/Program.cs
@@ -0,0 +1,63 @@
+﻿using CsvHelper;
+using CustomerCsv.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CustomerCsv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                object lockToread = new object();
+
+                List<CustomersCsv> lCustomers;
+
+                lock (lockToread)
+                {
+                    TextReader reader = new StreamReader("Customers.csv", Encoding.GetEncoding("iso-8859-1"));
+                    var csvReader = new CsvReader(reader);
+                    var customers = csvReader.GetRecords<CustomersCsv>();
+
+                    lCustomers = customers.ToList();
+                }
+
+                var context = new NorthwindContext(); using (context)
+                {
+
+                    var listaCustomersdb = context.CustomersCsv.ToList();
+
+                    foreach (var item in lCustomers)
+                    {
+                        if (listaCustomersdb.Exists(c => c.Id == item.Id))
+                        {
+                            Console.WriteLine("Customer with Id: " + item.Id +" already exist.");
+                        }
+                        else
+                        {
+                            context.CustomersCsv.Add(item);
+                            Console.WriteLine("Customer: " + item.Name + " just added.");
+                        }
+                    }
+
+                    context.SaveChanges();
+
+                    Console.WriteLine("Pulse ENTER para terminar.");
+                    Console.ReadLine();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                Console.ReadLine();
+            }
+        }
+    }
+}
